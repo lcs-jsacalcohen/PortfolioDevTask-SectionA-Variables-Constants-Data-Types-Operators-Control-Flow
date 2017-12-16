64cde23dee6c046317cbb0abc99da68e19efc3ad
Russell Gordon <jsacalcohen@lcs.on.ca>
2017-12-16 11:37:25 -0500

    Finished

--- Portfolio Development Task.playground/Pages/Question 5.xcplaygroundpage/Contents.swift
+++ Portfolio Development Task.playground/Pages/Question 5.xcplaygroundpage/Contents.swift
@@ -15,8 +15,12 @@ let isMale = true
 let neverEverSarcastic = true
 
 // Answer question 5 below
-
-
+if true {
+    "Must be Braeckman!"
+}
+if false {
+    "Hmm, need more data to be sure."
+}
 /*:
  ## Now share your understanding
  
